00f6f47ceffefa28274f5ee14382b45008404295
Leaping <lp471242389@qq.com>
2015-03-11 10:31:49 +0800

    Initial Commit

---
+++ PayTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayTest
+//
+//  Created by lipeng on 15-3-11.
+//  Copyright (c) 2015年 Kuparts.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayTest
+//
+//  Created by lipeng on 15-3-11.
+//  Copyright (c) 2015年 Kuparts.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
